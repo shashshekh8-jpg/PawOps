a1df1aacd91269996e7a2689f8ff667e7e5492c2
Shashwat Shekhar <shashshekh8@gmail.com>
2026-03-08 02:55:54 +0530

    Update tailwind.config.ts

--- FRONTEND/tailwind.config.ts
+++ FRONTEND/tailwind.config.ts
@@ -2,6 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  safelist: [
+    'bg-blue-500',
+    'bg-teal',
+    'bg-accent',
+    'bg-coral',
+    'w-full',
+    'w-3/4',
+    'w-1/2',
+    'w-1/3'
+  ],
   theme: {
     extend: {
       colors: {
@@ -16,4 +26,3 @@ const config: Config = {
   plugins: [],
 };
 export default config;
-
